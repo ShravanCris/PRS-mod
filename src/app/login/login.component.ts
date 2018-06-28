e5e0767a0182a4ee6ea9ce8b4e7cb93190b929f4
Shravan <shravankulkarni786@gmail.com>
2018-06-28 17:06:19 +0530

    checking for more than one credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginModel } from '../models/login.model';
+import { Router } from '@angular/router'
 import { UserService } from '../user.service';
 import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   user: LoginModel = new LoginModel();
   loginForm: FormGroup;
   
-  constructor(private formBuilder: FormBuilder,private userService : UserService) { }
+  constructor(private formBuilder: FormBuilder,private userService : UserService,private router : Router) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
@@ -46,22 +47,47 @@ export class LoginComponent implements OnInit {
       // }
       let username : String;
       let password : String;
+      let flag : Number;
+      flag=0;
       username = this.user.email;
       password = this.user.password;
-      console.log(username+ " " + password);
       this.userService.authenticateUser(username,password)
 			.subscribe(response => {
 				for(let user of response.data){
-					if(this.user.email == user.username && 
-							this.user.password ==user.password){
-						alert("Authenticated User!!");
-            window.location.href = '/conc-card'; 
-						
-					}else{
-						alert("User NOT Authenticated!!")
-					}
+          // console.log(user.username);
+          // console.log(user.password);
+					// if(this.user.email == user.username && 
+					// 		this.user.password ==user.password){
+					// 	alert("Authenticated User!!");
+          //   //window.location.href = '/conc-card'; 
+          //   this.router.navigate(['conc-card']);
+          //   break;
+					// }else{
+					// 	alert("User NOT Authenticated!!")
+          // }
+          
+          
+          if(this.user.email== user.username){
+            if(this.user.password == user.password)
+            {
+              
+              this.router.navigate(['conc-card']);
+              flag=1;
+              break;
+            }
+            else{
+              alert("Invalid password");
+              break;
+            }
+          }
+          else{
+            alert("Invalid user");
+            break;
+          }
+         
 				}
       });
+      
     }
 
 }
